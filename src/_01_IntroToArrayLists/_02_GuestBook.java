bb882492f5e45ea190412ed970f177767fb8621a
varnit05 <league@WTS22.attlocal.net>
2019-02-11 18:25:26 -0800

    i did GuestBook iPod shuffle

--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,15 +1,58 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
-	
-	
-	
+	JFrame frame;
+	JPanel panel;
+	JButton but1;
+	JButton but2;ArrayList<String> 
+	void createUi() {
+		frame = new JFrame();
+		panel = new JPanel();
+		but1 = new JButton();
+		but2 = new JButton();
+		frame.add(panel);
+		panel.add(but1);
+		panel.add(but2);
+		but1.setText("Add Names");
+		but2.setText("View Names");
+		frame.setVisible(true);
+		frame.pack();
+		but1.addActionListener(this);
+		but2.addActionListener(this);
+	}
+
+	public static void main(String[] args) {
+		new _02_GuestBook().createUi();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == but1) {
+			JOptionPane.showInputDialog("type a name");
+		} else if (e.getSource() == but2) {
+
+		}
+	}
+
 }
--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,29 +1,41 @@
 package _01_IntroToArrayLists;
 
 import java.util.ArrayList;
-import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 //Copyright The League of Amazing Programmers, 2015
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle {
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				Song s = new Song(null);
-			
-				
-				
+		Song s = new Song("demo.mp3");
+		s.play();
+
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
 		 * "Surprise Me!" button that will play a random song when it is clicked. * If
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
+		 * 
 		 */
-		
+
 	}
-	
+
 	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton but = new JButton();
 		new _06_IPodShuffle();
+		ArrayList<Song> sickbeats = new ArrayList<Song>();
+		frame.add(but);
+		frame.setSize(800, 800);
+		but.setText("Suprise Me !");
+		frame.setVisible(true);
+		frame.pack();
+
 	}
 }
