6c1cb48ec9d9521ce667547bc85798867c4d2dc2
mi <League@AdministorsiMac.attlocal.net>
2019-03-10 12:55:56 -0700

    Did stacks

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,13 +9,23 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
+		Stack<Double> a = new Stack<Double> ();
 		
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		for (int i = 0; i < 100; i++) {
+			a.push((double) i);
+			
+		}
 		
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		JOptionPane.showInputDialog("type 2 number between 0 and 100");
+		
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
+		for (int i = 0; i < args.length; i++) {
+			
+		}
 		
 		
 		//   EXAMPLE:
