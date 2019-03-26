0816a60ab97d7eda9e074f9e860c6e3dfb58bc27
varnit05 <league@WTS22.attlocal.net>
2019-03-25 18:27:05 -0700

    started hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,56 @@
 package _04_HangMan;
 
-public class HangMan{
-	
-	
-	
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements KeyListener {
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	Stack<String> randomStack = new Stack<String>();
+	String wordNum = JOptionPane.showInputDialog("type a number in between 1-266");
+	int word = Integer.parseInt(wordNum);
+	// for(int i = 0;)
+
+	public static void main(String[] args) {
+		new HangMan().createUI();
+	}
+
+	void createUI() {
+		frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel();
+
+		frame.add(panel);
+		panel.add(label);
+
+		frame.addKeyListener((KeyListener) this);
+		frame.setSize(800, 800);
+		frame.setVisible(true);
+		frame.pack();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
